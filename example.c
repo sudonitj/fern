712c13277ad4448a4ddc49d9ef862e9fba01f917
RishiAhuja <www.rishiahuja@gmail.com>
2025-05-10 00:07:36 +0530

    feat(shapes): bresenham lines, circles and rectangles

--- example.c
+++ example.c
@@ -6,8 +6,33 @@
 
 static uint32_t pixels [HEIGHT*WIDTH];
 
+void fline_ineff(uint32_t* pixels, size_t px_height, size_t px_width, uint32_t color, int x1, int y1, int x2, int y2) {
+    float m = (y2 - y1) / (x2 - x1);
+
+    for (int y = 0; y < px_height; y++){
+        for (int x = 0; x < px_width; x++){
+            if((x1-x2) !=0){
+                if((y - y1) == m * (x - x1)){
+                    pixels[y * px_width + x] = color;
+                }
+            }else{
+
+            }
+        }
+        
+    }
+}
+
+
+
 int main () {
-    ffill(pixels, HEIGHT, WIDTH, 0xFF0000FF); // Fill with red
+    int rw = 200;
+    int rh = 100;
+    int r = 50;
+    ffill(pixels, HEIGHT, WIDTH, 0xFF202020);
+    frect(pixels, HEIGHT, WIDTH, 0xFF00FF00, WIDTH/2 - rw/2, HEIGHT/2 - rh/2, rw, rh);
+    fcircle(pixels, HEIGHT, WIDTH, 0x00000FF, r, r, r);
+    fline(pixels, HEIGHT, WIDTH, 0xFFFF0000, 10, 10, 50, 50, 3);
     fsave_ppm(pixels, WIDTH, HEIGHT, "fern.ppm");
     return 0;
 }
--- fern.c
+++ fern.c
@@ -44,4 +44,58 @@ Errno fsave_ppm(uint32_t* pixels, size_t width, size_t height, const char* filen
     return result;
 }
 
+
+void frect(uint32_t* pixels, size_t height, size_t width, uint32_t color, size_t x, size_t y, size_t w, size_t h) {
+    for (int dx = 0; dx < (int) h; ++dx) {
+        for (int dy = 0; dy < (int) w; ++dy) {
+            if (x + dy < width && y + dx < height) {
+                pixels[(y + dx) * width + (x + dy)] = color;
+            }
+        }
+    }
+}
+
+void fcircle(uint32_t* pixels, size_t height, size_t width, uint32_t color, size_t cx, size_t cy, size_t r) {
+    for (int x = -r; x <= (int)r; x++){
+        for(int y = -r; y <= (int) r; y++){
+            if (x*x + y*y <= (int) (r*r)){
+                int px = cx + x;
+                int py = cy + y;
+                if (px < (int)width && py < (int)height){
+                    pixels[py * width + px] = color;
+                }
+            }
+        }
+    }
+}
+
+void fline(uint32_t* pixels, size_t px_height, size_t px_width, uint32_t color, int x1, int y1, int x2, int y2, int thickness){
+    int t = thickness;
+
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = x1 < x2 ? 1 : -1;
+    int sy = y1 < y2 ? 1 : -1;
+
+    int err = (dx - dy);
+
+    while(1){
+        if (x1 >= 0 && x1 < px_width && y1 >= 0 && y1 < px_height) {
+            // pixels[y1 * px_width + x1] = color; 
+            fcircle(pixels, px_height, px_width, color, x1, y1, t);
+        }
+
+        if(x1 == x2 && y1 == y2) break;
+        if(err > -dy){
+            err -= dy;
+            x1 += sx;
+        }
+
+        if(err < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
 #endif // FERN_H_
